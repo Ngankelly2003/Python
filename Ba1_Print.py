a398bd284646523debeec40b1b3acd43c264f0f8
Ngankelly2003 <nganguyenkxtb2003@gmail.com>
2023-04-17 23:46:07 +0700

    first commit

---
+++ Ba1_Print.py
@@ -0,0 +1,8 @@
+print("Tu hoc lap trinh Python")
+
+print("Xin","chao", "bae", sep=",")
+
+print('Xin','chao',end=':')
+print('Robot')
+
+print('Tên = {0}, Họ = {1}'.format('Ngân','Nguyễn'))
---
+++ Bai2_NhapDL.py
@@ -0,0 +1,11 @@
+
+'''
+input("Nhập vào một con số: ")
+a = input('Nhập vào a = ')
+print('a = {0}'.format(a))
+'''
+
+
+ho = input("Nhập họ: ")
+ten = input("Nhập tên: ")
+print('Xin chào ,',ho,ten)
